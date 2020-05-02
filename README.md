86907afc90cf6068d6a226166f4c65483c80fc81
黄磊 <huanglei@higohappy.com>
2020-05-03 01:01:32 +0800

    ADD: 链表翻转

--- README.md
+++ README.md
@@ -7,10 +7,11 @@
 * [快速排序](https://github.com/3-Stone/php-algorithm/blob/master/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F.php)
 * 归并排序
 * 桶排
-* [二分查找](https://github.com/3-Stone/php-algorithm/blob/master/%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE)
+* [二分查找](https://github.com/3-Stone/php-algorithm/blob/master/%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.php)
 * [php实现单链表](https://github.com/3-Stone/php-algorithm/blob/master/php%E5%AE%9E%E7%8E%B0%E5%8D%95%E9%93%BE%E8%A1%A8.php)
-* 链表翻转
+* [链表翻转](https://github.com/3-Stone/php-algorithm/blob/master/%E9%93%BE%E8%A1%A8%E7%BF%BB%E8%BD%AC.php)
 * 判断链表是否有环
 * 链表合并，合并后保持有序
 * 计算字符串中最大回文串
 * 链表倒数第k个节点
+* 无重复字符的最长子串
---
+++ 链表翻转.php
@@ -0,0 +1,110 @@
+<?php
+class Node
+{
+    public $data;
+    public $next;
+
+    public function __construct($data, $next)
+    {
+        $this->data = $data;
+        $this->next = $next;
+    }
+}
+
+class SingleLinkList
+{
+    private $headNode;      // 头节点
+    private $len;           // 节点数量
+
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * 链表初始化（生成头结点、初始化链表长度）
+     */
+    private function init()
+    {
+        $this->headNode = new Node(null, null);
+        $this->len = 0;
+    }
+
+    public function getHeadNode()
+    {
+        return $this->headNode;
+    }
+
+    /**
+     * 插入节点
+     *
+     * @param $data
+     * @param int $prevIndex 插入前一节点位置，0：插入头结点后，也就是第一个节点，1：第一个节点后（头结点的后一个的后一个）
+     * @return bool
+     */
+    public function insert($data, $prevIndex = 0)
+    {
+        if (!is_integer($prevIndex) || $prevIndex < 0 || $prevIndex > $this->len) {
+            return false;
+        }
+
+        $node = $this->getHeadNode();
+
+        $i = 0;
+
+        // 遍历链表，找到对应节点
+        while ($node->next !== null) {
+            if ($prevIndex === $i) {
+                break;
+            }
+
+            $node = $node->next;
+            $i++;
+        }
+
+        $newNode = new Node($data, $node->next);
+        $node->next = $newNode;
+        $this->len++;
+        return true;
+    }
+
+    // ... 省略链表其他操作，详细请看“php实现单链表”
+
+    public function reverseList()
+    {
+
+        // 先拿到整个链表
+        $headNode = $this->getHeadNode();
+
+        // 头结点赋值给$p
+        $p = $headNode;
+        // 链表中第一个元素赋值给$q
+        $q = $headNode->next;
+
+        // 让头结点变成尾节点（这里可以理解成另起了一个链表，且只有一个尾节点）
+        $p->next = null;
+
+        // 对原链表剩余节点进行迭代，不断的指向上面尾节点（也就是原链表中节点重新指向这个新链表）
+        // Ps：可以理解成$p在另一个链表上，$q在原链表上，在迭代过程中，让$p不断迁移，$q不断后移，直到$q没有后继节点，也就是到达表尾，翻转结束
+        while ($q !== null) {
+            $r = $q->next;      // 保留原链表的下一个节点，用于下次迭代使用
+            $q->next = $p;      // 让原链表中的节点，指向新链表
+            $p = $q;            // $p前移
+            $q = $r;            // 选中下一个节点，准备下一次迭代
+        }
+
+        return $p;
+    }
+}
+
+$singleLinkList = new SingleLinkList();
+
+$singleLinkList->insert('123');
+$singleLinkList->insert('456', 1);
+$singleLinkList->insert('789', 2);
+
+$reverseList = $singleLinkList->reverseList();
+
+print_r('<pre>');
+print_r($reverseList);
+print_r('</pre>');
